bc4bd138be4e596809571976260f13d00f5b9b65
yegor smertenko <ye.smertenko@gmail.com>
2023-10-25 13:25:01 -0700

    add Softmax output layer activation and add support for activation arg

--- activation.py
+++ activation.py
@@ -1,10 +1,23 @@
 import numpy as np
 
 
+# Used in hidden layers
 class ReLU:
     def forward(self, X: int):
         vec_max = np.vectorize(max)
         self.output = vec_max(0, X)
+        return self.output
+
+    def backward(self, X: int):
+        pass
+
+
+# Used in the output layer
+class Softmax:
+    def forward(self, X: int):
+        exp_vals = np.exp(X - np.max(X, axis=1, keepdims=True))
+        self.output = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
+        return self.output
 
     def backward(self, X: int):
         pass
--- neuron.py
+++ neuron.py
@@ -1,22 +1,25 @@
 import numpy as np
 
-from activation import ReLU
+from activation import ReLU, Softmax
 
 
 np.random.seed(0)
 
 
 class LayerDense:
-    def __init__(self, n_inputs: int, n_neurons: int):
+    def __init__(self, n_inputs: int, n_neurons: int, activation: object):
         # n_inputs x n_neurons = T(n_neurons x n_inputs)
         # Hence we take care of transpose for the dot product
         # 0.10 * to ensure we're between 0.1 and -.1
+        self.activation = activation()
         self.size = n_neurons
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs: list):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        output = np.dot(inputs, self.weights) + self.biases
+        self.output = self.activation.forward(output)
+        return self.output
 
 
 def main():
@@ -25,23 +28,22 @@ def main():
         [2.0, 5.0, -1.0, 2.0],
         [-1.5, 2.7, 3.3, -0.8],
     ]
-    layer1 = LayerDense(len(X[0]), 5)
-    layer2 = LayerDense(layer1.size, 2)
-    layer3 = LayerDense(layer2.size, 2)
+    layer1 = LayerDense(len(X[0]), 5, activation=ReLU)
+    layer2 = LayerDense(layer1.size, 2, activation=ReLU)
+    layer3 = LayerDense(layer2.size, 2, activation=Softmax)
 
-    activation = ReLU()
+    # activation = ReLU()
 
     layer1.forward(X)
-    activation.forward(layer1.output)
 
-    layer2.forward(activation.output)
-    activation.forward(layer2.output)
-    print(activation.output)
+    layer2.forward(layer1.output)
 
-    layer3.forward(activation.output)
-    activation.forward(layer3.output)
+    layer3.forward(layer2.output)
 
-    print(activation.output)
+    print(layer1.output)
+    print(layer2.output)
+    print(layer3.output)
+
     pass
 
 
